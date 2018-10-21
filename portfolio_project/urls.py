f90f02c50c4e087f9c250266d41f58fa1cbad625
MADELINE PETERS <madelinepet@hotmail.com>
2018-10-20 21:37:32 -0700

    More scaffolding complete. Templates for home view created and a base.html. Getting error cannot translate host name db to address: nodename nor servname provided, or not known when trying to create superuser.

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('budgets/', include('budgets.urls')),
+    # path('api/v1/', include('budget_api.urls')),
 ]
