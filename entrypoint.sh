5578f29c9e78dbb63799f653f2f258e2dcfd34e1
MADELINE PETERS <madelinepet@hotmail.com>
2018-11-16 15:32:35 -0800

    Removed docker completely, so changed .env to change db host, role, etc and deleted docker compose, dockerfile and entrypoint

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cd /src
-python3 manage.py collectstatic --noinput
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py runserver 0.0.0.0:8000
