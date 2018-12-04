b03918f0070d9ea8e3fb5b6dd18639006983c4ae
MADELINE PETERS <madelinepet@hotmail.com>
2018-12-03 16:11:16 -0800

    Working on the tone analysis for the news. Added message describing the view as a WIP.

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -9,8 +9,7 @@ def vis_view(request, data=None):
     is sent to the template and can be accessed there
     """
     data = get_news()
-    # tone = analyze()
-
+    tone = analyze()
     context = {
         'articles': []
     }
@@ -22,7 +21,7 @@ def vis_view(request, data=None):
         new_entry.append({'source': article['source']['name']})
         new_entry.append({'date_published': str(dateutil.parser.parse(article['publishedAt']))[:10]})
         new_entry.append({'url': article['url']})
-        # new_entry.append(tone), but with correct format
+        new_entry.append(tone)
         context['articles'].append(new_entry)
 
     return render(request, 'news/vis.html', context)
