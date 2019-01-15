65cbaf38ec46fb9434075d347b8ec15ec36ede18
MADELINE PETERS <madelinepet@hotmail.com>
2019-01-15 09:46:49 -0800

    missing files

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import vis_view
+from .views import game_view
 
 urlpatterns = [
-    path('vis', vis_view, name='vis'),
+    path('game', game_view, name='game'),
 ]
